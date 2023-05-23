05c172330b67955391db60dfc1b5fd2731ddb608
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-05-23 17:02:59 +0000

    first change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import './css/App.css';
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>teste</h1>
     </div>
-  );
+  )
 }
 
 export default App;
