08032cdb0e022f8e7430fa38687bfb0bc546c427
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-05-26 21:03:30 +0000

    new title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
+import { FiSearch } from 'react-icons/fi'
+import "./css/styles.css"
 function App() {
   return(
-    <div className="App">
-      <h1>teste</h1>
+    <div className="container">
+      <h1 className="title">EY Ferramentas</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite a ferramenta" />
+        
+        <button className="buttonSearch">
+          <FiSearch size={25} color ="#FFF" />
+        </button>
+      </div>
     </div>
   )
 }
