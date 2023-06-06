60b70f5b5c9861989a4dc1c69c11ac7ec16bf449
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-06-06 16:58:29 +0000

    commit de mudanças de requisições

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import {useState} from 'react';
 import { FiSearch } from 'react-icons/fi'
 import "./css/styles.css"
+
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    alert("valor do input" + input)
+  }
+  
+
   return(
+
     <div className="container">
-      <h1 className="title">EY Ferramentas</h1>
+      <h1 className="title">Tools</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite a ferramenta" />
+        <input type="text" placeholder="Digite a ferramenta" value={input} onChange={(e) => setInput(e.target.value) } />
         
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color ="#FFF" />
         </button>
       </div>
+
+       <main className="main">
+      <h2>Nome tals </h2>
+      </main>
     </div>
   )
 }
